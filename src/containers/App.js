f08fced119148a484b0d010e6d246f544165f113
alexlefk <64595001+alexlefk@users.noreply.github.com>
2020-05-10 10:51:14 +0300

    bazoil friends app

---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/Searchbox";
+import Scroll from "../components/Scroll";
+import "./app.css";
+import ErroBoundry from "../components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState({
+          robots: users,
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    this.setState({
+      searchfield: event.target.value,
+    });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+
+    return !robots.length ? (
+      <div className="tc">
+        <h1> Loading </h1>;
+      </div>
+    ) : (
+      <div className="tc">
+        <h1 className="f2"> Bazoil Friends </h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErroBoundry>
+            <CardList robots={filteredRobots} />
+          </ErroBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
